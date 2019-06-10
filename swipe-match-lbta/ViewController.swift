7979351dcd1f05f55531497ff059f01c9589b7fe
Karlis Berzins <berzinsh.karlis@gmail.com>
2019-06-10 10:59:22 +0300

    Initial commit

---
+++ swipe-match-lbta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swipe-match-lbta
+//
+//  Created by Karlis Berzins on 10/06/2019.
+//  Copyright © 2019 Karlis Berzins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
