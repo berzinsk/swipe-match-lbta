1dded9425d6ef2304c38d7b89545b609bdd549ec
Karlis Berzins <berzinsh.karlis@gmail.com>
2019-07-01 16:08:26 +0300

    7 - intro mvvm card view models

---
+++ swipe-match-lbta/ViewModels/CardViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  CardViewModel.swift
+//  swipe-match-lbta
+//
+//  Created by Karlis Berzins on 01/07/2019.
+//  Copyright © 2019 Karlis Berzins. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+    // we'll define the properties that our view will display/render out
+    let imageName: String
+    let attributedString: NSAttributedString
+    let textAlignment: NSTextAlignment
+}
