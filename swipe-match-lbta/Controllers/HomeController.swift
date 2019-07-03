6dc00e011774089abcbd7ceddcfc34e82134e181
Karlis Berzins <berzinsh.karlis@gmail.com>
2019-07-03 19:29:11 +0300

    9 - advertiser model protocol oriented programming

--- swipe-match-lbta/Controllers/HomeController.swift
+++ swipe-match-lbta/Controllers/HomeController.swift
@@ -14,10 +14,16 @@ class HomeController: UIViewController {
     let cardsDeckView = UIView()
     let buttonsStackView = HomeBottomControlsStackView()
 
-    let cardViewModels = [
-        User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "lady5c").toCardViewModel(),
-        User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c").toCardViewModel()
-    ]
+    let cardViewModels: [CardViewModel] = {
+        let producers: [ProducesCardViewModel] = [
+            User(name: "Kelly", age: 23, profession: "Music DJ", imageName: "lady5c"),
+            User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c"),
+            Advertiser(title: "Slide Out Menu", brandName: "Lets Build That App", posterPhotoName: "slide_out_menu_poster")
+        ]
+
+        let viewModels = producers.map { $0.toCardViewModel() }
+        return viewModels
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,9 +35,7 @@ class HomeController: UIViewController {
     fileprivate func setupDummyCards() {
         cardViewModels.forEach { cardVM in
             let cardView = CardView()
-            cardView.imageView.image = UIImage(named: cardVM.imageName)
-            cardView.informationLabel.attributedText = cardVM.attributedString
-            cardView.informationLabel.textAlignment = cardVM.textAlignment
+            cardView.cardViewModel = cardVM
 
             cardsDeckView.addSubview(cardView)
             cardView.fillSuperview()
---
+++ swipe-match-lbta/Model/Advertiser.swift
@@ -0,0 +1,22 @@
+//
+//  Advertiser.swift
+//  swipe-match-lbta
+//
+//  Created by Karlis Berzins on 03/07/2019.
+//  Copyright © 2019 Karlis Berzins. All rights reserved.
+//
+
+import UIKit
+
+struct Advertiser: ProducesCardViewModel {
+    let title: String
+    let brandName: String
+    let posterPhotoName: String
+
+    func toCardViewModel() -> CardViewModel {
+        let attributedString = NSMutableAttributedString(string: title, attributes: [.font: UIFont.systemFont(ofSize: 34, weight: .heavy)])
+        attributedString.append(NSAttributedString(string: "\n\(brandName)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .bold)]))
+
+        return CardViewModel(imageName: posterPhotoName, attributedString: attributedString, textAlignment: .center)
+    }
+}
