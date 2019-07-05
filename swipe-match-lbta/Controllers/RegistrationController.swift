52f3c4c8e8ea84eb4198ed8b317af32fd7a40a55
Karlis Berzins <berzinsh.karlis@gmail.com>
2019-07-05 20:28:07 +0300

    14 - keyboard management notification observers

--- swipe-match-lbta/Controllers/RegistrationController.swift
+++ swipe-match-lbta/Controllers/RegistrationController.swift
@@ -54,27 +54,51 @@ class RegistrationController: UIViewController {
         button.setTitle("Register", for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .heavy)
         button.backgroundColor = #colorLiteral(red: 0.8235294118, green: 0, blue: 0.3254901961, alpha: 1)
-        button.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
         button.setTitleColor(.white, for: .normal)
-        button.layer.cornerRadius = 24
+        button.layer.cornerRadius = 22
         button.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
 
         return button
     }()
 
+    lazy var stackView = UIStackView(arrangedSubviews: [
+        selectPhotoButton,
+        fullNameTextField,
+        emailTextField,
+        passwordTextField,
+        registerButton
+    ])
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupGradientLayer()
+        setupLayout()
+        setupNotificationObservers()
+        setupTapGesture()
+    }
 
-        let stackView = UIStackView(arrangedSubviews: [
-            selectPhotoButton,
-            fullNameTextField,
-            emailTextField,
-            passwordTextField,
-            registerButton
-        ])
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
 
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    // MARK:- Private
+
+    fileprivate func setupGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+        let topColor = #colorLiteral(red: 0.9921568627, green: 0.3568627451, blue: 0.3725490196, alpha: 1)
+        let bottomColor = #colorLiteral(red: 0.8980392157, green: 0, blue: 0.4470588235, alpha: 1)
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.locations = [0, 1]
+
+        view.layer.addSublayer(gradientLayer)
+        gradientLayer.frame = view.bounds
+    }
+
+    fileprivate func setupLayout() {
         view.addSubview(stackView)
 
         stackView.axis = .vertical
@@ -87,15 +111,36 @@ class RegistrationController: UIViewController {
         stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
-    fileprivate func setupGradientLayer() {
-        let gradientLayer = CAGradientLayer()
-        let topColor = #colorLiteral(red: 0.9921568627, green: 0.3568627451, blue: 0.3725490196, alpha: 1)
-        let bottomColor = #colorLiteral(red: 0.8980392157, green: 0, blue: 0.4470588235, alpha: 1)
-        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
-        gradientLayer.locations = [0, 1]
+    fileprivate func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
 
-        view.layer.addSublayer(gradientLayer)
-        gradientLayer.frame = view.bounds
+    @objc fileprivate func handleKeyboardShow(notification: Notification) {
+        guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+        let keyboardFrame = value.cgRectValue
+        print(keyboardFrame)
+
+        // Figure out the gap between register button to the bottom of the screen
+        let bottomSpace = view.frame.height - stackView.frame.origin.y - stackView.frame.height
+        let difference = keyboardFrame.height - bottomSpace
+        view.transform = CGAffineTransform(translationX: 0, y: -difference - 8)
+    }
+
+    fileprivate func setupTapGesture() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss)))
+    }
+
+    @objc fileprivate func handleTapDismiss() {
+        view.endEditing(true)
+    }
+
+    @objc fileprivate func handleKeyboardHide() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.view.transform = .identity
+        })
     }
 }
 
